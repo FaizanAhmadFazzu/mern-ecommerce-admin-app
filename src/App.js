8fcebf3ff3cffe759de595c160092be284c5dc64
Faizan Ahmad <faizan@opskube.com>
2021-07-15 19:25:46 +0530

    show and add categories

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Signup from './containers/Signup';
 import { isUserLoggedIn} from './actions'
 import Products from './containers/Products';
 import Orders from './containers/Orders';
+import Category from './containers/Category';
 
 function App() {
 
@@ -28,6 +29,7 @@ function App() {
     <div className="App">  
       <Switch>
         <PrivateRoute path="/" exact component={Home} />
+        <PrivateRoute path="/category" exact component={Category} />
         <PrivateRoute path="/products" component={Products} />
         <PrivateRoute path="/orders" component={Orders} />
         <Route path="/signin" component={Signin} />

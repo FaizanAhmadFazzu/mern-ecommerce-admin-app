d5c44566d5b83092c3660c470fcabf664c069391
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-08-03 17:51:08 +0530

    Fix JWT Expire Login Issue

--- src/actions/category.action.js
+++ src/actions/category.action.js
@@ -2,69 +2,70 @@ import axios from "../helpers/axios";
 import { categoryConstants } from "./constants";
 
 export const getAllCategory = () => {
-    return async dispatch => {
-        const res = await axios.get('/category/getCategory');
-        console.log(res);
-        dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST });
-        if (res.status == 200) {
-
-            const { categoryList } = res.data;
-            dispatch({
-                type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
-                payload: { categories: categoryList }
-            });
-        } else {
-            dispatch({
-                type: categoryConstants.GET_ALL_CATEGORIES_FAILURE,
-                payload: { error: res.data.error }
-            });
-        }
+  return async (dispatch) => {
+    const res = await axios.get("/category/getCategory");
+    console.log(res);
+    dispatch({ type: categoryConstants.GET_ALL_CATEGORIES_REQUEST });
+    if (res.status == 200) {
+      const { categoryList } = res.data;
+      dispatch({
+        type: categoryConstants.GET_ALL_CATEGORIES_SUCCESS,
+        payload: { categories: categoryList },
+      });
+    } else {
+      dispatch({
+        type: categoryConstants.GET_ALL_CATEGORIES_FAILURE,
+        payload: { error: res.data.error },
+      });
     }
-}
+  };
+};
 
 export const addCategory = (form) => {
-    return async dispatch => {
-        dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST })
-        const res = await axios.post('/category/create', form);
-        if (res.status === 201) {
-            dispatch({
-                type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
-                payload: { category: res.data.category }
-            });
-        } else {
-            dispatch({
-                type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
-                payload: res.data.error
-            });
-        }
-       
+  return async (dispatch) => {
+    dispatch({ type: categoryConstants.ADD_NEW_CATEGORY_REQUEST });
+    try {
+      const res = await axios.post("/category/create", form);
+      if (res.status === 201) {
+        dispatch({
+          type: categoryConstants.ADD_NEW_CATEGORY_SUCCESS,
+          payload: { category: res.data.category },
+        });
+      } else {
+        dispatch({
+          type: categoryConstants.ADD_NEW_CATEGORY_FAILURE,
+          payload: res.data.error,
+        });
+      }
+    } catch (error) {
+      console.log(error.response);
     }
-}
+  };
+};
 
 export const updateCategories = (form) => {
-    return async dispatch => {
-        const res = await axios.post('/category/update', form);
-        if (res.status === 201) {
-            console.log(res);
-            return true;
-        } else {
-            console.log(res);
-        }
-       
+  return async (dispatch) => {
+    const res = await axios.post("/category/update", form);
+    if (res.status === 201) {
+      console.log(res);
+      return true;
+    } else {
+      console.log(res);
     }
-}
+  };
+};
 
 export const deleteCategories = (ids) => {
-    return async dispatch => {
-        const res = await axios.post('/category/delete', {
-            payload: {
-                ids
-            }
-        });
-        if (res.status == 201) {
-            return true;
-        } else {
-            return false;
-        }
+  return async (dispatch) => {
+    const res = await axios.post("/category/delete", {
+      payload: {
+        ids,
+      },
+    });
+    if (res.status == 201) {
+      return true;
+    } else {
+      return false;
     }
-}
+  };
+};

092882092698db027650239c5b4e5a52e23f83d9
Faizan Ahmad Khan <ahmadfaizan1604@gmail.com>
2021-09-02 20:07:25 +0530

    Update Order Status

---
+++ src/actions/order.action.js
@@ -0,0 +1,24 @@
+import axios from "../helpers/axios";
+
+export const updateOrder = (payload) => {
+  return async (dispatch) => {
+    // dispatch({ type: pageConstants.CREATE_PAGE_REQUEST });
+    try {
+      const res = await axios.post("/order/update", payload);
+      console.log(res);
+      if (res.status == 200) {
+        // dispatch({
+        //     type: pageConstants.CREATE_PAGE_SUCCESS,
+        //     payload: { page: res.data.page }
+        // })
+      } else {
+        // dispatch({
+        //     type: pageConstants.CREATE_PAGE_FAILURE,
+        //     payload: { error: res.data.error }
+        // })
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
--- src/containers/Orders/index.js
+++ src/containers/Orders/index.js
@@ -1,46 +1,99 @@
-import React from "react";
+import React, {  useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateOrder } from "../../actions";
 import Layout from "../../components/Layout";
 import Card from "../../components/UI/Card";
-import "./style.css"
+import "./style.css";
 
 const Orders = () => {
+  const order = useSelector((state) => state.order);
+  const [type, setType] = useState('');
+  const dispatch = useDispatch();
+
+  const onOrderUpdate = (orderId) => {
+    const payload = {
+      orderId,
+      type
+    }
+    dispatch(updateOrder(payload));
+  }
   return (
     <Layout sidebar>
-      <Card headerLeft="ORDER_ID">
-        <div
-          style={{
-            boxSizing: "border-box",
-            padding: "100px",
-          }}
-        >
-          <div className="orderTrack">
-            <div className="orderStatus">
-                <div className="point">
-                    <div className="orderInfo">
-                        <div className="status">Ordered</div>
-                        <div className="date">Fri, 2021</div>
-                    </div>
+      {order.orders.map((orderItem, index) => (
+        <Card key={index} headerLeft={orderItem._id}>
+          <div
+            style={{
+              boxSizing: "border-box",
+              padding: "100px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div className="orderTrack">
+              <div className="orderStatus">
+                <div className="point"></div>
+                <div className="orderInfo">
+                  <div className="status">Ordered</div>
+                  <div className="date">Fri, 2021</div>
                 </div>
+              </div>
+              <div className="orderStatus">
+                <div className="point"></div>
+                <div className="orderInfo">
+                  <div className="status">Packed</div>
+                  <div className="date">Fri, 2021</div>
+                </div>
+              </div>
+              <div className="orderStatus">
+                <div className="point"></div>
+                <div className="orderInfo">
+                  <div className="status">Ordered</div>
+                  <div className="date">Fri, 2021</div>
+                </div>
+              </div>
+              <div className="orderStatus">
+                <div className="point"></div>
+                <div className="orderInfo">
+                  <div className="status">Ordered</div>
+                  <div className="date">Fri, 2021</div>
+                </div>
+              </div>
             </div>
-            <div className="orderStatus">
-                <div className="point">
-                    <div className="orderInfo">
-                        <div className="status">Packed</div>
-                        <div className="date">Fri, 2021</div>
-                    </div>
-                </div>
+
+            {/* Select input to apply order action */}
+            <div
+              style={{
+                boxSizing: "border-box",
+                padding: "0 50px",
+              }}
+            >
+              <select onChange={(e) => setType(e.target.value)}>
+                <option value={""}>select status</option>
+                {orderItem.orderStatus.map((status) => {
+                  return (
+                    <>
+                      {!status.isCompleted ? (
+                        <option key={status.type} value={status.type}>
+                          {status.type}
+                        </option>
+                      ) : null}
+                    </>
+                  );
+                })}
+              </select>
             </div>
-            <div className="orderStatus">
-                <div className="point">
-                    <div className="orderInfo">
-                        <div className="status">Ordered</div>
-                        <div className="date">Fri, 2021</div>
-                    </div>
-                </div>
+
+            <div
+              style={{
+                boxSizing: "border-box",
+                padding: "0 50px",
+              }}
+            >
+              <button onClick={() => onOrderUpdate(orderItem._id)}>Confirm</button>
             </div>
           </div>
-        </div>
-      </Card>
+        </Card>
+      ))}
     </Layout>
   );
 };
